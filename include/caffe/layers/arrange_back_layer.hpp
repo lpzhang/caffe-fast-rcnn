c11aec62092ecdfca373c0b5a7c953a97516a552
lpzhang <lpzhang2012@gmail.com>
2018-01-07 13:55:23 +0800

    arrange_back

---
+++ include/caffe/layers/arrange_back_layer.hpp
@@ -0,0 +1,55 @@
+#ifndef CAFFE_ARRANGE_BACK_LAYER_HPP_
+#define CAFFE_ARRANGE_BACK_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+namespace caffe {
+
+/**
+ * @brief Takes a Blob and arranges it back along spatial dimension,
+ *        outputting arranged back Blob results.
+ *
+ * TODO(dox): thorough documentation for Forward, Backward, and proto params.
+ */
+template <typename Dtype>
+class ArrangeBackLayer : public Layer<Dtype> {
+ public:
+  explicit ArrangeBackLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "ArrangeBack"; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+  virtual inline int MinTopBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  // virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+  //     const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  // virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+  //     const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  
+  int num_;
+  int channels_;
+  int height_;
+  int width_;
+  int region_height_;
+  int region_width_;
+
+  int stride_;
+  ArrangeParameter_ArrangementMode arrangement_;
+};
+
+}  // namespace caffe
+
+#endif  // CAFFE_ARRANGE_BACK_LAYER_HPP_
